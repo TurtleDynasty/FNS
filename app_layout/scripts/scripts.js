9c96570150094a791011aef42b1f796d6f678cb7
JT-Barrett <jtbarrett0804@hotmail.com>
2016-11-27 11:02:24 -0700

    Create scripts.js

---
+++ app_layout/scripts/scripts.js
@@ -0,0 +1,95 @@
+function init_spinner(){
+    var opts = {
+    lines: 9 // The number of lines to draw
+    , length: 5 // The length of each line
+    , width: 6 // The line thickness
+    , radius: 41 // The radius of the inner circle
+    , scale: 0.75 // Scales overall size of the spinner
+    , corners: 1 // Corner roundness (0..1)
+    , color: '#000' // #rgb or #rrggbb or array of colors
+    , opacity: 0.05 // Opacity of the lines
+    , rotate: 0 // The rotation offset
+    , direction: 1 // 1: clockwise, -1: counterclockwise
+    , speed: 0.8 // Rounds per second
+    , trail: 42 // Afterglow percentage
+    , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+    , zIndex: 2e9 // The z-index (defaults to 2000000000)
+    , className: 'spinner' // The CSS class to assign to the spinner
+    , top: '50%' // Top position relative to parent
+    , left: '50%' // Left position relative to parent
+    , shadow: false // Whether to render a shadow
+    , hwaccel: false // Whether to use hardware acceleration
+    , position: 'absolute' // Element positioning
+    }
+    var target = document.getElementsByClassName('widget2');
+    var spinner = new Spinner(opts).spin(target[0]);
+}
+
+function init_sunburst() {
+    var width = 960,
+    height = 700,
+    radius = (Math.min(width, height) / 2) - 10;
+
+var formatNumber = d3.format(",d");
+
+var x = d3.scaleLinear()
+    .range([0, 2 * Math.PI]);
+
+var y = d3.scaleSqrt()
+    .range([0, radius]);
+
+var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+var partition = d3.partition();
+
+var arc = d3.arc()
+    .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x0))); })
+    .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x1))); })
+    .innerRadius(function(d) { return Math.max(0, y(d.y0)); })
+    .outerRadius(function(d) { return Math.max(0, y(d.y1)); });
+
+
+    var svg = d3.select(".widget2")
+        .append("div")
+        .classed("svg-container", true)
+        .append("svg")
+        .attr("preserveAspectRatio", "xMinYMin meet")
+        .attr("viewBox", "0 0 960 700")
+        .attr("id", "vis")
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + (height / 2) + ")");
+
+    d3.json("scripts/flare.json", function(error, root) {
+      if (error) throw error;
+      
+      root = d3.hierarchy(root);
+      root.sum(function(d) { return d.size; });
+      svg.selectAll("path")
+          .data(partition(root).descendants())
+        .enter().append("path")
+          .attr("d", arc)
+          .style("fill", function(d) { return color((d.children ? d : d.parent).data.name); })
+          .on("click", click)
+        .append("title")
+          .text(function(d) { return d.data.name + "\n" + formatNumber(d.value); });
+    });
+
+    function click(d) {
+      svg.transition()
+          .duration(750)
+          .tween("scale", function() {
+            var xd = d3.interpolate(x.domain(), [d.x0, d.x1]),
+                yd = d3.interpolate(y.domain(), [d.y0, 1]),
+                yr = d3.interpolate(y.range(), [d.y0 ? 20 : 0, radius]);
+            return function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); };
+          })
+        .selectAll("path")
+          .attrTween("d", function(d) { return function() { return arc(d); }; });
+    }
+
+    d3.select(self.frameElement).style("height", height + "px");
+    d3.select("#vis").style("margin-top", "-25px");
+
+    //change vis title
+    d3.select("#vis-title").html("Sunburst");
+}
