7abe6bf0361f7a3a6af24b520f6284eb199b2ac9
BRD-E <BRD-E@outlook.com>
2016-12-30 15:08:04 -0700

    Cleaned it up a lot!
    Made better error handling
    Made better print statements
    Suppressed the unknown table warnings
    Fixed an issue with some data not being inserted correctly

--- DB scripts/mysql_csv_directory_import.py
+++ DB scripts/mysql_csv_directory_import.py
@@ -11,6 +11,8 @@ import csv
 import MySQLdb
 import os
 import string
+import warnings
+warnings.filterwarnings("ignore")
 
 def main():
 
@@ -28,10 +30,10 @@ def main():
     # Make sure the database tsmgui11 is created on your MySQL space
     # Change these variables as needed
     mydb = MySQLdb.connect(host='127.0.0.1',
-        user='root',
-        port=30000,
+        user='',
+        port=3306,
         passwd='',
-        db='tsmgui11')
+        db='TSMGUI11')
 
     if mydb:
         print("Connection Successful")
@@ -42,44 +44,63 @@ def main():
         CHANGE THE USER NAME
         PLACE THE UNZIPPED database folder tsmgui11 in a folder called DATABASE on your desktop
     """
-    PATH1 = "C:/Users/BRD-E/Desktop/DATABASE/tsmgui11"
-    PATH2 = "C:\Users\BRD-E\Desktop\DATABASE\tsmgui11"
+    PATH1 = "C:/Users/BRD-E/Desktop/DATABASE/TSMGUI11"
+    PATH2 = "C:\Users\BRD-E\Desktop\DATABASE\TSMGUI11"
     #Pass over each file, importing it to the database
     #Change this path to the path of the UNCOMPRESSED tsmgui11 on your machine
     for file_name in os.listdir(PATH1):
         if file_name.endswith('.csv'):
+            rollback_flag = False
+            table_name = file_name[:-4]
+            print("-\n--\n--- starting on: " + table_name + ".csv")
+            print("--- Opening CSV File")
+            cursor = mydb.cursor()
             try:
-                print("\n===============================================\n" + file_name)
-                cursor = mydb.cursor()
-                table_name = file_name[:-4]
                 with open(PATH1 + '/' + file_name, 'r') as f:
                     row1 = f.readline()
                     sql_create += addfields(row1, table_name)
+                print("--- CSV Opened successfully!")
+            except Exception as e:
+                print("--- ISSUE: Opening and adding header data!!!")
+                print(e)
+                rollback_flag = True
+            try:
                 if row1.__len__() > 0:
                     #Drop the table if it exists
-                    print("\nExecuting MySQL Command:\n" + sql_drop.format(table_name))
-                    cursor.execute(sql_drop.format(table_name))
+                    print("--- Dropping Table")
+                    cursor.execute(sql_drop.format(table_name.upper()))
                     cursor.close()
-
-                    #Build the table creation command and execute it
-                    cursor = mydb.cursor()
-                    print("\nExecuting MySQL Command:\n" + sql_create.format(table_name, table_name))
-                    cursor.execute(sql_create.format(table_name, table_name))
-                    cursor.close()
-
-                    #Insert the CSV data into the new table
-                    cursor = mydb.cursor()
-                    print("\nExecuting MySQL Command:\n" + sql_insert.format(PATH2 + '/' + file_name, table_name.lower()))
-                    cursor.execute(sql_insert.format(PATH2 + '/' + file_name, table_name.lower()))
-                    cursor.close()
-                    mydb.commit()
-                sql_create = """"""
-
-            except Exception:
-                # Rollback in case there is any error
-                print("\n====================Exception======================\n" + file_name)
-                sql_create = """"""
+                    print("--- Table Drop successfully!")
+            except Exception as e:
+                print("--- ISSUE: Dropping table")
+                print(e)
+                rollback_flag = True
+            try:
+                #Build the table creation command and execute it
+                print("--- Creating Table")
+                cursor = mydb.cursor()
+                cursor.execute(sql_create.format(table_name, table_name))
+                cursor.close()
+            except Exception as e:
+                print("--- ISSUE: Creating table")
+                print(e)
+                rollback_flag = True
+            try:
+                #Insert the CSV data into the new table
+                print("--- Filling table with data!")
+                cursor = mydb.cursor()
+                cursor.execute(sql_insert.format(PATH1 + '/' + file_name, table_name.lower()))
+                cursor.close()
+                mydb.commit()
+                print("--- Data was filled sucessfully!")
+            except Exception as e:
+                print("----- ISSUE: Filling table")
+                print(e)
+                rollback_flag = True
+            if (rollback_flag):
                 mydb.rollback()
+            sql_create = """"""
+            print("---\n--\n-")
 
 def addfields(csv_headings, table_name):
     """ This helper method tokenizes takes the first row of a csv file, tokenizes it and adds each as a new column in
