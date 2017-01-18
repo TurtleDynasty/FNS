55775a748e7d818782cb38487837d4486cf5c4e1
Joshua Melton <BRD-E@outlook.com>
2017-01-18 13:43:24 -0700

    Fixed filepath
    Fixed Jquery / bootstrap -v error

--- app_layout/scripts/scripts.js
+++ app_layout/scripts/scripts.js
@@ -1,5 +1,5 @@
 function init_spinner(){
-    var opts = {
+  var opts = {
     lines: 9 // The number of lines to draw
     , length: 5 // The length of each line
     , width: 6 // The line thickness
@@ -20,101 +20,103 @@ function init_spinner(){
     , shadow: false // Whether to render a shadow
     , hwaccel: false // Whether to use hardware acceleration
     , position: 'absolute' // Element positioning
-    }
-    var target = document.getElementsByClassName('widget2');
-    var spinner = new Spinner(opts).spin(target[0]);
-    $("#vis-title").html("Select a Visualization");
+  }
+  var target = document.getElementsByClassName('widget2');
+  var spinner = new Spinner(opts).spin(target[0]);
+  $("#vis-title").html("Select a Visualization");
 }
 
 function clear_vis(){
-    $( ".spinner" ).remove();
-    $( ".svg-container" ).remove();
+  $( ".spinner" ).remove();
+  $( ".svg-container" ).remove();
 }
 
 //https://bl.ocks.org/maybelinot/5552606564ef37b5de7e47ed2b7dc099
 function init_sunburst() {
-    clear_vis();
-    var width = d3.select(".widget2").node().getBoundingClientRect().width,
-    height = d3.select(".widget2").node().getBoundingClientRect().height,
-    radius = (Math.min(width, height) / 2) - 10;
+  clear_vis();
+  var width = d3.select(".widget2").node().getBoundingClientRect().width,
+  height = d3.select(".widget2").node().getBoundingClientRect().height,
+  radius = (Math.min(width, height) / 2) - 10;
 
-var formatNumber = d3.format(",d");
+  var formatNumber = d3.format(",d");
 
-var x = d3.scaleLinear()
-    .range([0, 2 * Math.PI]);
+  var x = d3.scaleLinear()
+  .range([0, 2 * Math.PI]);
 
-var y = d3.scaleSqrt()
-    .range([0, radius]);
+  var y = d3.scaleSqrt()
+  .range([0, radius]);
 
-var color = d3.scaleOrdinal(d3.schemeCategory20);
+  var color = d3.scaleOrdinal(d3.schemeCategory20);
 
-var partition = d3.partition();
+  var partition = d3.partition();
 
-var arc = d3.arc()
-    .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x0))); })
-    .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x1))); })
-    .innerRadius(function(d) { return Math.max(0, y(d.y0)); })
-    .outerRadius(function(d) { return Math.max(0, y(d.y1)); });
+  var arc = d3.arc()
+  .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x0))); })
+  .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x1))); })
+  .innerRadius(function(d) { return Math.max(0, y(d.y0)); })
+  .outerRadius(function(d) { return Math.max(0, y(d.y1)); });
 
 
-    var svg = d3.select(".widget2")
-        .append("div")
-        .classed("svg-container", true)
-        .append("svg")
-        .attr("preserveAspectRatio", "xMinYMin meet")
-        .attr("viewBox", "0 0 " + width + " " + height)
-        .attr("id", "vis")
-        .append("g")
-        .attr("transform", "translate(" + width / 2 + "," + (height / 2) + ")");
-
-    d3.json("http://localhost/pullData.php?queryNumber=1&format=CSV", function(error, root) {
-      if (error) throw error;
-
-      root = d3.hierarchy(root);
-      root.sum(function(d) { return d.size; });
-      svg.selectAll("path")
-          .data(partition(root).descendants())
-        .enter().append("path")
-          .attr("d", arc)
-          .style("fill", function(d) { return color((d.children ? d : d.parent).data.name); })
-          .on("click", click)
-        .append("title")
-          .text(function(d) { return d.data.name + "\n" + formatNumber(d.value); });
-    });
-
-    function click(d) {
-      svg.transition()
-          .duration(750)
-          .tween("scale", function() {
-            var xd = d3.interpolate(x.domain(), [d.x0, d.x1]),
-                yd = d3.interpolate(y.domain(), [d.y0, 1]),
-                yr = d3.interpolate(y.range(), [d.y0 ? 20 : 0, radius]);
-            return function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); };
-          })
-        .selectAll("path")
-          .attrTween("d", function(d) { return function() { return arc(d); }; });
+  var svg = d3.select(".widget2")
+  .append("div")
+  .classed("svg-container", true)
+  .append("svg")
+  .attr("preserveAspectRatio", "xMinYMin meet")
+  .attr("viewBox", "0 0 " + width + " " + height)
+  .attr("id", "vis")
+  .append("g")
+  .attr("transform", "translate(" + width / 2 + "," + (height / 2) + ")");
+  d3.json("scripts/falseData.json", function(error, root) {
+    if (error)
+    {
+      throw error;
     }
 
-    d3.select(self.frameElement).style("height", height + "px");
-    d3.select("#vis").style("margin-top", "-25px");
+    root = d3.hierarchy(root);
+    root.sum(function(d) { return d.size; });
+    svg.selectAll("path")
+    .data(partition(root).descendants())
+    .enter().append("path")
+    .attr("d", arc)
+    .style("fill", function(d) { return color((d.children ? d : d.parent).data.name); })
+    .on("click", click)
+    .append("title")
+    .text(function(d) { return d.data.name + "\n" + formatNumber(d.value); });
+  });
 
-    //change vis title
-    d3.select("#vis-title").html("Sunburst");
+  function click(d) {
+    svg.transition()
+    .duration(750)
+    .tween("scale", function() {
+      var xd = d3.interpolate(x.domain(), [d.x0, d.x1]),
+      yd = d3.interpolate(y.domain(), [d.y0, 1]),
+      yr = d3.interpolate(y.range(), [d.y0 ? 20 : 0, radius]);
+      return function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); };
+    })
+    .selectAll("path")
+    .attrTween("d", function(d) { return function() { return arc(d); }; });
+  }
+
+  d3.select(self.frameElement).style("height", height + "px");
+  d3.select("#vis").style("margin-top", "-25px");
+
+  //change vis title
+  d3.select("#vis-title").html("Sunburst");
 }
-
+console.log("document ready?")
 $(document).ready(function(){
-    var sample = [{ id: 0, text: 'TSMGUI12' }, { id: 1, text: 'TSMGUI11' }, { id: 2, text: 'File Domain' }, { id: 3, text: 'TSMGUI18' }];
-    $(".select-objects").select2({
-      data: sample
-    });
+  var sample = [{ id: 0, text: 'TSMGUI12' }, { id: 1, text: 'TSMGUI11' }, { id: 2, text: 'File Domain' }, { id: 3, text: 'TSMGUI18' }];
+  $(".select-objects").select2({
+    data: sample
+  });
 
-    $(".select-objects").select2({
-      placeholder: "Click Here to Select Objects"
-    });
+  $(".select-objects").select2({
+    placeholder: "Click Here to Select Objects"
+  });
 
-    $(".generate-button").on("click", function(){
-        clear_vis();
-        init_spinner();
-        setTimeout(init_sunburst, 1500);
-    });
+  $(".generate-button").on("click", function(){
+    clear_vis();
+    init_spinner();
+    setTimeout(init_sunburst, 1500);
+  });
 });
