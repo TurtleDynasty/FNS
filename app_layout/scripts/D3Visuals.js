7981bc98c3748228e38e5ebc1fbe6dab8da964fc
Jack "JT" Barrett <jtbarrett0804@hotmail.com>
2017-03-17 13:37:19 -0700

    Added Heatmap

--- app_layout/scripts/D3Visuals.js
+++ app_layout/scripts/D3Visuals.js
@@ -319,181 +319,225 @@ function init_sunburst() {
 
 function init_pie_test() {
   var url = "http://localhost/";
-	var margin = {top: 50, right: 50, bottom: 50, left: 50};
-	var width = d3.select(".widget2").node().getBoundingClientRect().width,
-	    height = d3.select(".widget2").node().getBoundingClientRect().height;
-	var outerRadius = Math.min(width, height) / 4;
-	var innerRadius = outerRadius - outerRadius/3;
-	var letterLineLength = 11;
-	var lineToLabelEnd = outerRadius * .2;
-	var lineToLabelStart = outerRadius; //innerRadius + (outerRadius -innerRadius)/2;
-	var color = d3.scaleOrdinal()
-	.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-	var arc = d3.arc()
-	.outerRadius(outerRadius)
-	.innerRadius(innerRadius);
-	var labelArc = d3.arc()
-	.outerRadius(outerRadius)
-	.innerRadius(innerRadius);
-	var pie = d3.pie()
-	.sort(null)
-	.value(function(d) { return d.count; });
-	var svg = d3.select(".widget2").append("div")
-          .classed("svg-container", true).append("svg")
-	.attr("width", width)
-	.attr("height", height)
-	.append("g")
-	.attr("transform", "translate(" + width / 2  + "," + height / 2 + ")");
-	var total = 0;
-	d3.csv("csvs/data.csv", type, function(error, data)
-	{
-		if (error)
-		{
-			throw error;
-		}
-		var toggle = 0;
-		data = parseData(data);
-		var g = svg.selectAll(".arc")
-		.data(pie(data))
-		.enter().append("g")
-		.attr("class", "arc");
-		g.append("path")
-		.attr("d", arc)
-		.style("fill", function(d) { return color(d.data.name); })
-		.on("mouseenter", function(d) {
-			if (toggle == 0)
-			{
-				text = g.append("text")
-					.attr("transform", arc.centroid(d))
-					.attr("dy", ".5em")
-					.style("text-anchor", "middle")
-					.style("fill", "black")
-					.style("font-size", "35px")
-					.attr("class", "on")
-					.text(" ");
-				toggle += 1;
-			}
-			text.text(d.data.name + ": " + d.data.count);
-		})
-		.on("mouseout", function(d) {
-				text.text("Total: " + total);
-		});
-
-    for(i = 0; i < data.length; i++){
-
-			total += data[i].count;
-    }
-    d3.select("path").each(function(p){
-      total = total + p.data.count;
-    })
-		// text
-		g.append("text")
-		.attr("text-anchor", "middle")
-		.attr("x", function(d) {
-			var a = d.startAngle + (d.endAngle - d.startAngle)/2 - Math.PI/2;
-			if (Math.cos(a) * (outerRadius + lineToLabelEnd) > 0)
-			{
-				return Math.cos(a) * (outerRadius + lineToLabelEnd) + letterLineLength*d.data.name.length/2;
-			}
-			else
-			{
-				return Math.cos(a) * (outerRadius + lineToLabelEnd) - letterLineLength*d.data.name.length/2;
-			}
-		})
-		.attr("y", function(d) {
-			var a = d.startAngle + (d.endAngle - d.startAngle)/2 - Math.PI/2;
-			return Math.sin(a) * (outerRadius + lineToLabelEnd) - 5;
-		})
-		.style("font-size", "14px")
-		.text(function(d) { return d.data.name; });
-		// pie2labelline
-		g.append("line")
-		.attr("x1", function(d) {
-			var a = d.startAngle + (d.endAngle - d.startAngle)/2 - Math.PI/2;
-			return Math.cos(a) * (lineToLabelStart);
-		})
-		.attr("y1", function(d) {
-			var a = d.startAngle + (d.endAngle - d.startAngle)/2 - Math.PI/2;
-			return Math.sin(a) * (lineToLabelStart);
-		})
-		.attr("x2", function(d) {
-			var a = d.startAngle + (d.endAngle - d.startAngle)/2 - Math.PI/2;
-			return Math.cos(a) * (outerRadius + lineToLabelEnd);
-		})
-		.attr("y2", function(d) {
-			var a = d.startAngle + (d.endAngle - d.startAngle)/2 - Math.PI/2;
-			return Math.sin(a) * (outerRadius + lineToLabelEnd);
-		})
-		.attr("stroke-width", 2)
-        .attr("stroke", "black");
-		// label underline
-		g.append("line")
-		.attr("x1", function(d) {
-			var a = d.startAngle + (d.endAngle - d.startAngle)/2 - Math.PI/2;
-			return Math.cos(a) * (outerRadius + lineToLabelEnd);
-		})
-		.attr("y1", function(d) {
-			var a = d.startAngle + (d.endAngle - d.startAngle)/2 - Math.PI/2;
-			return Math.sin(a) * (outerRadius + lineToLabelEnd);
-		})
-		.attr("x2", function(d) {
-			var a = d.startAngle + (d.endAngle - d.startAngle)/2 - Math.PI/2;
-			if (Math.cos(a) * (outerRadius + lineToLabelEnd) > 0)
-			{
-				return Math.cos(a) * (outerRadius + lineToLabelEnd) + letterLineLength*d.data.name.length;
-			}
-			else
-			{
-				return Math.cos(a) * (outerRadius + lineToLabelEnd) - letterLineLength*d.data.name.length;
-			}
-		})
-		.attr("y2", function(d) {
-			var a = d.startAngle + (d.endAngle - d.startAngle)/2 - Math.PI/2;
-			return Math.sin(a) * (outerRadius + lineToLabelEnd);
-		})
-		.attr("stroke-width", 2)
-		.attr("stroke", "black");
-		svg.attr("transform", "translate(" + (width/ 2) + "," + (height/ 2) + ")");
-	});
-	function parseData(data)
-	{
-		var compressed = compressArray(data);
-		var parsedData = customFilterData(compressed);
-		return parsedData;
-	}
-	function compressArray(original)
-	{
-		var copy = [original[0]]
-		for(i = 1; i < original.length; i++)
-		{
-			var notFound = true;
-			for (j = 0; j < copy.length; j ++)
-			{
-				if (original[i].name == copy[j].name)
-				{
-					notFound = false;
-					copy[j].count += original[i].count;
-				}
-			}
-			if (notFound)
-			{
-				copy.push(original[i]);
-			}
-		}
-		return copy;
-	}
-	function customFilterData(data)
-	{
-		// Custom data filters go here.
-		var copy = []
-		for(i = 0; i < data.length; i++)
-		{
-			// Filter goes here. console.log(data[i]) to know the poperties of said data
-			console.log(data[i]);
-			copy.push(data[i]);
-		}
-		return copy;
-	}
+  	var margin = {top: 50, right: 50, bottom: 50, left: 50};
+  	var width = 1000 - margin.left - margin.right;
+  	var height = 900 - margin.top - margin.bottom;
+  	var innerRadius = Math.min(width, height) / 4;
+  	var outerRadius = Math.min(width, height) / 3;
+  	// ---- label Text STUFF
+  	var labelFontSize = "14px"
+  	// -------------------
+  	// ---- Inner Text STUFF
+  	var units = "objects";
+  	var numDecimalPlaces = 3;
+  	var innerFontSize = "35px";
+  	var labelXOffset = "0em";
+  	var labelYOffset = "-.5em";
+  	var topXOffset = "0em";
+  	var topYOffset = ".5em";
+  	var bottomXOffset = "0em";
+  	var bottomYOffset = "1.5em";
+  	// --------------------
+  	// --- linelabels STUFF
+  	var letterLineLength = 11;
+  	var lineToLabelEnd = outerRadius * .2;
+  	var lineToLabelStart = outerRadius;
+  	// --------------------
+  	var color = d3.scaleOrdinal()
+  	.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+  	var arc = d3.arc()
+  	.outerRadius(outerRadius)
+  	.innerRadius(innerRadius);
+  	var labelArc = d3.arc()
+  	.outerRadius(outerRadius)
+  	.innerRadius(innerRadius);
+  	var pie = d3.pie()
+  	.sort(null)
+  	.value(function(d) { return d.count; });
+    var svg = d3.select(".widget2").append("div")
+            .classed("svg-container", true)
+            .append("svg")
+  	.attr("width", width)
+  	.attr("height", height)
+  	.append("g")
+  	.attr("transform", "translate(" + width / 2  + "," + height / 2 + ")");
+  	function type(d)
+  	{
+  		d.count = +d.count;
+  		return d;
+  	}
+  	var total = 0;
+  	d3.csv("csvs/data.csv", type, function(error, data)
+  	{
+  		if (error)
+  		{
+  			throw error;
+  		}
+  		data = parseData(data);
+  		g = svg.selectAll(".arc")
+  		.data(pie(data))
+  		.enter().append("g")
+  		.attr("class", "arc");
+  		createTextBoxes(g);
+  		g.append("path")
+  		.attr("d", arc)
+  		.style("fill", function(d) { return color(d.data.name); })
+  		.on("mouseenter", function(d) {
+  			innerLabelEnter(d);
+  		})
+  		.on("mouseout", function(d) {
+  			innerLabelExit(d);
+  		});
+  		// text
+  		g.append("text")
+  		.attr("text-anchor", "middle")
+  		.attr("x", function(d) {
+  			var a = calculateAngle(d);
+  			var quadrant = Math.cos(a) * (outerRadius + lineToLabelEnd);
+  			return quadrantCalculator(quadrant, letterLineLength*d.data.name.length/2);
+  		})
+  		.attr("y", function(d) {
+  			var a = calculateAngle(d);
+  			return Math.sin(a) * (outerRadius + lineToLabelEnd) - 5;
+  		})
+  		.style("font-size", labelFontSize)
+  		.text(function(d) { return d.data.name; });
+  		// pie2labelline
+  		g.append("line")
+  		.attr("x1", function(d) {
+  			var a = calculateAngle(d);
+  			return Math.cos(a) * (lineToLabelStart);
+  		})
+  		.attr("y1", function(d) {
+  			var a = calculateAngle(d);
+  			return Math.sin(a) * (lineToLabelStart);
+  		})
+  		.attr("x2", function(d) {
+  			var a = calculateAngle(d);
+  			return Math.cos(a) * (outerRadius + lineToLabelEnd);
+  		})
+  		.attr("y2", function(d) {
+  			var a = calculateAngle(d);
+  			return Math.sin(a) * (outerRadius + lineToLabelEnd);
+  		})
+  		.attr("stroke-width", 2)
+          .attr("stroke", "black");
+  		// label underline
+  		g.append("line")
+  		.attr("x1", function(d) {
+  			var a = calculateAngle(d);
+  			return Math.cos(a) * (outerRadius + lineToLabelEnd);
+  		})
+  		.attr("y1", function(d) {
+  			var a = calculateAngle(d);
+  			return Math.sin(a) * (outerRadius + lineToLabelEnd);
+  		})
+  		.attr("x2", function(d) {
+  			var a = calculateAngle(d);
+  			var quadrant = Math.cos(a) * (outerRadius + lineToLabelEnd);
+  			return quadrantCalculator(quadrant, letterLineLength*d.data.name.length);
+  		})
+  		.attr("y2", function(d) {
+  			var a = calculateAngle(d);
+  			return Math.sin(a) * (outerRadius + lineToLabelEnd);
+  		})
+  		.attr("stroke-width", 2)
+  		.attr("stroke", "black");
+  		svg.attr("transform", "translate(" + (width/ 2 + margin.left) + "," + (height/ 2 + margin.top) + ")");
+  		setTextBoxes(data);
+  	});
+  	function createTextBoxes(g)
+  	{
+  		text = g.append("text")
+  			.attr("dx", topXOffset)
+  			.attr("dy", topYOffset)
+  			.style("text-anchor", "middle")
+  			.style("fill", "black")
+  			.style("font-size", innerFontSize)
+  			.attr("class", "on")
+  			.text(" ");
+  		bottomText = g.append("text")
+  		.attr("dx", bottomXOffset)
+  		.attr("dy", bottomYOffset)
+  		.style("text-anchor", "middle")
+  		.style("fill", "black")
+  		.style("font-size", innerFontSize)
+  		.attr("class", "on")
+  		.text(" ");
+  		labelText = g.append("text")
+  		.attr("dx", labelXOffset)
+  		.attr("dy", labelYOffset)
+  		.style("text-anchor", "middle")
+  		.style("fill", "black")
+  		.style("font-size", innerFontSize)
+  		.attr("class", "on")
+  		.text(" ");
+  	}
+  	function setTextBoxes(data)
+  	{
+  		text.text(total + " " + units);
+  	}
+  	function quadrantCalculator(quadrant, offset)
+  	{
+  		if (quadrant > 0)
+  		{
+  			return quadrant + offset;
+  		}
+  		else
+  		{
+  			return quadrant - offset;
+  		}
+  	}
+  	function calculateAngle(data)
+  	{
+  		return data.startAngle + (data.endAngle - data.startAngle)/2 - Math.PI/2;
+  	}
+  	function innerLabelEnter(d)
+  	{
+  		text.text(d.data.count + "/" + total + " " + units);
+  		bottomText.text(((d.data.count/total)*100).toFixed(numDecimalPlaces) + "%");
+  		labelText.text(d.data.name);
+  	}
+  	function innerLabelExit(d)
+  	{
+  		text.text(total + " " + units);
+  		bottomText.text("");
+  		labelText.text("");
+  	}
+  	function parseData(data)
+  	{
+  		var compressed = compressArray(data);
+  		var parsedData = customFilterData(compressed);
+  		return parsedData;
+  	}
+  	function compressArray(original)
+  	{
+  		var copy = [original[0]]
+  		for(i = 1; i < original.length; i++)
+  		{
+  			var notFound = true;
+  			for (j = 0; j < copy.length; j ++)
+  			{
+  				if (original[i].name == copy[j].name)
+  				{
+  					notFound = false;
+  					total += original[i].count;
+  					copy[j].count += original[i].count;
+  				}
+  			}
+  			if (notFound)
+  			{
+  				total += original[i].count;
+  				copy.push(original[i]);
+  			}
+  		}
+  		return copy;
+  	}
+  	function customFilterData(data)
+  	{
+  		// Custom data filters go here.
+  		return data;
+  	}
 
   d3.select("#vis-title").html("Object Count by Container");
 }
@@ -576,3 +620,178 @@ svg.selectAll(".bar")
 
 d3.select("#vis-title").html("Storage Pool Test");
 }
+
+function init_heatmap() {
+  var colors = ["darkblue", "blue", "green", "seagreen", "greenyellow" , "yellow", "orange", "orangered", "red"];
+  d3.select(".widget2").append("div").classed("svg-container", true).append("svg").classed("heatmap", true).attr("width", 1336).attr("height", 768);
+  var svg = d3.select("svg");
+  var width = +svg.attr("width");
+  var height = +svg.attr("height");
+  var bubblePadding = 1;
+  var format = d3.format(",d");
+  createColorTable();
+  function createColorTable()
+  {
+  	var x = 0;
+  	colors.forEach(function(element)
+  	{
+  		var rectangle = svg.append("rect")
+  			.attr("x", x)
+  			.attr("y", height * .8)
+  			.attr("width", width * .90)
+  			.attr("height", height * .2)
+  			.attr("fill", element);
+  			x += width / colors.length;
+  	});
+  }
+  function grabColor(number, min, max)
+  {
+  	var divideColor = d3.scaleLinear().domain([min, max]).range([0, 1]);
+  	var pickColor = d3.scaleLinear().domain([.1, .2, .3, .4, .5, .6, .7, .8, .9]).range(colors);
+  	var color = divideColor(number);
+  	return pickColor(color);
+  }
+  var pack = d3.pack()
+  .size([width, height*.8])
+  .padding(bubblePadding);
+  var maxColor = 0;
+  var minColor = 9999999999999;
+  d3.csv("csvs/heatmap.csv", function(d) {
+  	d.value = +d.value;
+  	if (d.value > 1)
+  	{
+  		if (d.value < minColor)
+  		{
+  			minColor = d.value;
+  		}
+  		if (d.value > maxColor)
+  		{
+  			maxColor = d.value;
+  		}
+  		return d;
+  	}
+  }, function(error, classes) {
+  	if (error) throw error;
+  	var root = d3.hierarchy({children: classes})
+  	.sum(function(d) { return d.value; })
+  	.each(function(d) {
+  		if (id = d.data.id) {
+  			var id, i = id.lastIndexOf(".");
+  			d.id = id;
+  			d.package = id.slice(0, i);
+  			d.class = id.slice(i + 1);
+  		}
+  	});
+  	var node = svg.selectAll(".node")
+  	.data(pack(root).leaves())
+  	.enter().append("g")
+  	.attr("class", "node")
+  	.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+  	node.append("circle")
+  	.attr("id", function(d) { return d.id; })
+  	.attr("r", function(d) { return d.r; })
+  	.style("fill", function(d) { return grabColor(d.value, minColor, maxColor); });
+  	node.append("clipPath")
+  	.attr("id", function(d) { return "clip-" + d.id; })
+  	.append("use")
+  	.attr("xlink:href", function(d) { return "#" + d.id; });
+  	node.append("text")
+  	.attr("clip-path", function(d) { return "url(#clip-" + d.id + ")"; })
+  	.selectAll("tspan")
+  	.data(function(d) { return d.class.split(/(?=[A-Z][^A-Z])/g); })
+  	.enter().append("tspan")
+  	.attr("x", 0)
+  	.attr("y", function(d, i, nodes) { return 13 + (i - nodes.length / 2 - 0.5) * 10; })
+  	.text(function(d) { return d; });
+  	node.append("title")
+  	.text(function(d) { return d.id + "\n" + format(d.value); });
+  });
+
+
+  d3.select("#vis-title").html("Heat Map Test");
+}
+/*
+var colors = ["darkblue", "blue", "green", "seagreen", "greenyellow" , "yellow", "orange", "orangered", "red"];
+var svg = d3.select("svg");
+var width = +svg.attr("width");
+var height = +svg.attr("height");
+var bubblePadding = 1;
+var format = d3.format(",d");
+createColorTable();
+function createColorTable()
+{
+	var x = 0;
+	colors.forEach(function(element)
+	{
+		var rectangle = svg.append("rect")
+			.attr("x", x)
+			.attr("y", height * .8)
+			.attr("width", width * .90)
+			.attr("height", height * .2)
+			.attr("fill", element);
+			x += width / colors.length;
+	});
+}
+function grabColor(number, min, max)
+{
+	var divideColor = d3.scaleLinear().domain([min, max]).range([0, 1]);
+	var pickColor = d3.scaleLinear().domain([.1, .2, .3, .4, .5, .6, .7, .8, .9]).range(colors);
+	var color = divideColor(number);
+	return pickColor(color);
+}
+var pack = d3.pack()
+.size([width, height*.8])
+.padding(bubblePadding);
+var maxColor = 0;
+var minColor = 9999999999999;
+d3.csv("flare.csv", function(d) {
+	d.value = +d.value;
+	if (d.value > 1)
+	{
+		if (d.value < minColor)
+		{
+			minColor = d.value;
+		}
+		if (d.value > maxColor)
+		{
+			maxColor = d.value;
+		}
+		return d;
+	}
+}, function(error, classes) {
+	if (error) throw error;
+	var root = d3.hierarchy({children: classes})
+	.sum(function(d) { return d.value; })
+	.each(function(d) {
+		if (id = d.data.id) {
+			var id, i = id.lastIndexOf(".");
+			d.id = id;
+			d.package = id.slice(0, i);
+			d.class = id.slice(i + 1);
+		}
+	});
+	var node = svg.selectAll(".node")
+	.data(pack(root).leaves())
+	.enter().append("g")
+	.attr("class", "node")
+	.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+	node.append("circle")
+	.attr("id", function(d) { return d.id; })
+	.attr("r", function(d) { return d.r; })
+	.style("fill", function(d) { return grabColor(d.value, minColor, maxColor); });
+	node.append("clipPath")
+	.attr("id", function(d) { return "clip-" + d.id; })
+	.append("use")
+	.attr("xlink:href", function(d) { return "#" + d.id; });
+	node.append("text")
+	.attr("clip-path", function(d) { return "url(#clip-" + d.id + ")"; })
+	.selectAll("tspan")
+	.data(function(d) { return d.class.split(/(?=[A-Z][^A-Z])/g); })
+	.enter().append("tspan")
+	.attr("x", 0)
+	.attr("y", function(d, i, nodes) { return 13 + (i - nodes.length / 2 - 0.5) * 10; })
+	.text(function(d) { return d; });
+	node.append("title")
+	.text(function(d) { return d.id + "\n" + format(d.value); });
+});
+*/
